99119ed8102e7431ca523b091ab1a47ff6ef76fd
Sammit <sammit.dp.pal2000@gmail.com>
2022-03-28 12:36:34 +0530

    :white_check_mark: v2

--- src/component/Row.js
+++ src/component/Row.js
@@ -14,7 +14,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t1==='C/C++' && 'c' || t1==='Analysis' && 'analysis' || t1==='Array' && 'array' || t1==='Database Management System' && 'dbms' || t1==="Web Development" && 'web' || t1==="Profit and Loss" && 'pl' || t1==='Analogies' && 'an'
+      value: t1==='C/C++' && 'c' || t1==='Analysis' && 'analysis' || t1==='Array' && 'array' || t1==='Database Management System' && 'dbms' || t1==="Web Development" && 'web' || t1==="Profit and Loss" && 'pl' || t1==='Analogies' && 'an' || t1=='Engineering Maths for GATE' && 'emg'
     })
   }
   const videost2 = () =>{
@@ -25,7 +25,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t2==='Java' && 'java' || t2==='Backtracking' && 'backtracking' || t2==='Binary Search Tree' && 'bst' || t2==='Digital and Logic Design' && 'dld' || t2==='Mobile Development' && 'md' || t2==="Percentage" && 'percent' || t2==='Statement Assumption' && 'sm'
+      value: t2==='Java' && 'java' || t2==='Backtracking' && 'backtracking' || t2==='Binary Search Tree' && 'bst' || t2==='Digital and Logic Design' && 'dld' || t2==='Mobile Development' && 'md' || t2==="Percentage" && 'percent' || t2==='Statement Assumption' && 'sm' || t2==='Digital Logic for GATE' && 'dlg'
     })
   }
   const videost3 = () =>{
@@ -36,7 +36,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t3==='Python' && 'python' || t3==='Graph Algorithm' && 'graphalgo' || t3==='Linked List' && 'linked' || t3==='Computer Networks' && 'net' || t3==='Internet of Things' && 'iot' || t3==='Numbers' && 'numbers' || t3==='Blood Relation' && 'br'
+      value: t3==='Python' && 'python' || t3==='Graph Algorithm' && 'graphalgo' || t3==='Linked List' && 'linked' || t3==='Computer Networks' && 'net' || t3==='Internet of Things' && 'iot' || t3==='Numbers' && 'numbers' || t3==='Blood Relation' && 'br' || t3==='Computer Organition and Architecture for GATE' && 'coag'
     })
   }
   const videost4 = () =>{
@@ -47,7 +47,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t4==='Sorting' && 'sorting' || t4==='Stack' && 'stack' || t4 === 'Object Oriented Programming in JAVA' && 'oops' || t4 === 'Machine Learning' && 'ml' || t4==='Boats and Streams' && 'bs' || t4==='Syllogism' && 'sg'
+      value: t4==='Sorting' && 'sorting' || t4==='Stack' && 'stack' || t4 === 'Object Oriented Programming in JAVA' && 'oops' || t4 === 'Machine Learning' && 'ml' || t4==='Boats and Streams' && 'bs' || t4==='Syllogism' && 'sg' || t4==='Programming & Data Structure for GATE' && 'pdg'
     })
   }
   const videost5 = () =>{
@@ -58,7 +58,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t5==='String' && 'string' || t5=="Searching" && 'searching' || t5 === 'Operating System' && 'os' || t5==='Cyber Security' && 'cs' || t5==='Time and Distance' && 'td' || t5==='Sitting Arrangements' && 'sa'
+      value: t5==='String' && 'string' || t5=="Searching" && 'searching' || t5 === 'Operating System' && 'os' || t5==='Cyber Security' && 'cs' || t5==='Time and Distance' && 'td' || t5==='Sitting Arrangements' && 'sa' || t5==='Algorithms for GATE' && 'alg'
     })
   }
   const videost6 = () =>{
@@ -69,7 +69,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t6==='Queue' && 'queue' || t6=="Branch and Bound" && 'branchandbound' || t6==="Software Engineering" && 'se' || t6==='Cloud Computing' && 'cc' || t6==='Time and Work' && 'tw' || t6==='Direction Test' && 'dt'
+      value: t6==='Queue' && 'queue' || t6=="Branch and Bound" && 'branchandbound' || t6==="Software Engineering" && 'se' || t6==='Cloud Computing' && 'cc' || t6==='Time and Work' && 'tw' || t6==='Direction Test' && 'dt' || t6=='Theory of Computation for GATE' && 'tocg'
     })
   }
   const videost7 = () =>{
@@ -80,7 +80,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t7==='Graph' && 'graphs' || t7=="Greedy Algorithm" && 'greedy' || t7==='Ethical Hacking' && 'eh' || t7==='Problems on Ages' && 'pa' || t7==='Letter and Series' && 'ls'
+      value: t7==='Graph' && 'graphs' || t7=="Greedy Algorithm" && 'greedy' || t7==='Ethical Hacking' && 'eh' || t7==='Problems on Ages' && 'pa' || t7==='Letter and Series' && 'ls' || t7 == 'Compier Design for GATE' && 'cdg'
     })
   }
   const videost8 = () =>{
@@ -91,7 +91,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t8==='Hashing' && 'hashing' || t8==='Permutation and Combination' && 'pc'
+      value: t8==='Hashing' && 'hashing' || t8==='Permutation and Combination' && 'pc' || t8==='Operating System for GATE' && 'osg'
     })
   }
   const videost9 = () =>{
@@ -102,7 +102,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t9==='Tree' && 'tree' || t9==='Ratio and Proportion' && 'rp'
+      value: t9==='Tree' && 'tree' || t9==='Ratio and Proportion' && 'rp' || t9==='DBMS for GATE' && 'dbg'
     })
   }
   const videost10 = () =>{
@@ -113,7 +113,7 @@ function Row({heading, t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12}) {
     })
     dispatch({
       type: 'SET_VALUE',
-      value: t10==='Dynamic Programming' && 'dynamic' || t10==='Pipes and Cisterns' && 'cp'
+      value: t10==='Dynamic Programming' && 'dynamic' || t10==='Pipes and Cisterns' && 'cp' || t10 === 'Networking for GATE' && 'cng'
     })
   }
   const videost11 = () =>{
